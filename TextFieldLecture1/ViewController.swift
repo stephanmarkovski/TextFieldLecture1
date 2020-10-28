31f135097eb6519abbf6ae418fd2ff32c610489b
Stephan Markovski <smarkovski4271@stu.d214.org>
2020-10-28 12:41:34 -0700

    Added button and textfield to storyboard. Added code to print the text property from textfield.

--- TextFieldLecture1/ViewController.swift
+++ TextFieldLecture1/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenButtonTapped(_ sender: Any) {
+        let data = textfield.text!
+        print(data)
+    }
+    
 }
 
